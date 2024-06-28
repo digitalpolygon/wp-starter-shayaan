b0135617799efffab77224a15d1b68b2f616eec9
Shayaan <shayaanmomin.sm@gmail.com>
2024-06-28 15:58:21 +0530

    Added JSON for Menu Items

--- web/app/themes/dp-timber-theme/.storybook/preview.js
+++ web/app/themes/dp-timber-theme/.storybook/preview.js
@@ -3,6 +3,8 @@ import "bootstrap/scss/bootstrap.scss";
 import "bootstrap/dist/js/bootstrap.js";
 
 import '../components/style.scss';
+// CSS for Storybook Only
+import '../components/storybook/storybook.scss';
 
 // Add viewports for our example device widths.
 const deviceViewports = {
